0a31d898c7fcc286772e4fd269b2a9ec0e636c62
gabriel-estudo <milgabriel551@gmail.com>
2026-03-01 18:09:44 -0300

    calculadora feita

--- calculadora.php
+++ calculadora.php
@@ -6,7 +6,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $subtracao = $a - $b;
   $multiplicacao = $a * $b;
   $divisao = $a / $b;
-  $operacao = $_POST['operacao'];
 }
 //  echo 'Resultado: ' . $soma;
 //  http://localhost/daw/soma.php?a=2&b=3
@@ -20,27 +19,29 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   <form method='POST' action='calculadora.php'>
     a:<input type=text name='a'><br>
     b:<input type=text name='b'>
-    <input type=submit value='Somar'>
-    <input type=submit value='Subtrair'>
-    <input type=submit value='Multiplicar'>
-    <input type=submit value='Dividir'>
+    <input type=submit name="operacao" value='Somar'>
+    <input type=submit name="operacao" value='Subtrair'>
+    <input type=submit name="operacao" value='Multiplicar'>
+    <input type=submit name="operacao" value='Dividir'>
     <br><br>
     <?php
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-      if ($operacao == 'Somar') {
-        echo 'Resultado: ' . $soma;
-      } elseif ($operacao == 'Subtrair') {
-        echo '<br>Resultado: ' . $subtracao;
-      } elseif ($operacao == 'Multiplicar') {
-        echo '<br>Resultado: ' . $multiplicacao;
-      } elseif ($operacao == 'Dividir') {
-        echo '<br>Resultado: ' . $divisao;
+      if (isset($_POST['operacao'])) {
+        $operacao = $_POST['operacao'];
+
+        if ($operacao == 'Somar') {
+          echo 'Resultado: ' . $soma;
+        } elseif ($operacao == 'Subtrair') {
+          echo '<br>Resultado: ' . $subtracao;
+        } elseif ($operacao == 'Multiplicar') {
+          echo '<br>Resultado: ' . $multiplicacao;
+        } elseif ($operacao == 'Dividir') {
+          echo '<br>Resultado: ' . $divisao;
+        }
       }
     }
     ?>
-
 </body>
 
 </html
-
